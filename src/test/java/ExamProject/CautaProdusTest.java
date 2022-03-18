81a93bde53aeb5f62faecbdf95c7426856b00697
DorinDadu <puscasu.dorin@gmail.com>
2022-03-18 17:03:46 +0200

    modificari finale

--- src/test/java/ExamProject/CautaProdusTest.java
+++ src/test/java/ExamProject/CautaProdusTest.java
@@ -1,20 +1,15 @@
 package ExamProject;
 
 import Baza.Hooks;
-import Help.ElementMethods;
 import Pages.CautaProdusPage;
 import org.junit.Test;
 
-
 public class CautaProdusTest extends Hooks {
     public CautaProdusPage cautaProdusPage;
-    public ElementMethods elementMethods;
 
     @Test
     public void cautaprodusTest() {
         cautaProdusPage = new CautaProdusPage(getDriver());
-        elementMethods = new ElementMethods(getDriver());
-
         cautaProdusPage.cautaProduseProcess();
 
     }
--- src/test/java/Pages/CautaProdusPage.java
+++ src/test/java/Pages/CautaProdusPage.java
@@ -4,7 +4,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.List;
 
 public class CautaProdusPage extends BasePage {
@@ -19,8 +22,12 @@ public class CautaProdusPage extends BasePage {
     private WebElement enterElement;
     @FindBy(xpath = "//*[@id=\"__next\"]/div[2]/div[1]/main/ul/li/div[1]/div/div[2]/div[3]/a/span/span")
     private WebElement checkthecartElement;
-    @FindBy(css = "span[class='pl-1 -tracking-0.29']")
-    private WebElement cleanthecartElement;
+    @FindBy(xpath = "//*[@id=\"__next\"]/div[1]/div[1]/div/div/div[2]/div[1]/a/span[2]")
+    private WebElement continuaCumparaturileElement;
+    @FindBy(xpath = "//*[@id=\"notice-cookie-block\"]/div/button/span/span")
+    private WebElement acceptCokieElement;
+    @FindBy(xpath = "//*[@id=\"__next\"]/div[2]/div[1]/main/div[2]/div[1]/div[2]/div[2]/div[1]/div[2]/div/button")
+    private WebElement addtoCartElement;
 
     public void fillsearchfield(String value) {
         elementMethods.fillElement(searchElement, value);
@@ -31,7 +38,8 @@ public class CautaProdusPage extends BasePage {
     }
 
     public void produseOption(int index) {
-        List<WebElement> produseOption = driver.findElements(By.xpath("//ul[@class='Products  flex flex-wrap relative -mx-1 sm:-mx-2']/li/a/div[6]/button"));
+        new WebDriverWait(driver, Duration.ofSeconds(20)).until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//span[text()='Adauga in cos']")));
+        List<WebElement> produseOption = driver.findElements(By.xpath("//h1/..//span[text()='Adauga in cos']"));
         produseOption.get(index).click();
     }
 
@@ -39,12 +47,31 @@ public class CautaProdusPage extends BasePage {
         elementMethods.clickElement(checkthecartElement);
     }
 
-    public void cleanthecart() {
-        elementMethods.clickElement(cleanthecartElement);
+    public void continuaCumparaturile() {
+        elementMethods.clickElement(continuaCumparaturileElement);
+    }
+
+    public void bicicleteAdultiOption(int index) {
+        new WebDriverWait(driver, Duration.ofSeconds(20)).until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//span[text()='Adauga in cos']")));
+        List<WebElement> produseOption = driver.findElements(By.cssSelector("[class='Product-photoWrapper']"));
+        produseOption.get(index).click();
+
+    }
+
+    public void clickAddToCart() {
+        elementMethods.clickElement(addtoCartElement);
     }
 
     public void scrollBy() {
         elementMethods.scrollElement(0, 400);
+    }
+
+    public void scrollByAgain() {
+        elementMethods.scrollElement(0, 600);
+    }
+
+    public void clickAcceptCookie() {
+        elementMethods.clickElement(acceptCokieElement);
     }
 
     public void cautaProduseProcess() {
@@ -54,8 +81,16 @@ public class CautaProdusPage extends BasePage {
         scrollBy();
         elementMethods.waitImplicitElement(60);
         produseOption(0);
+        continuaCumparaturile();
+        fillsearchfield("Biciclete adulti");
+        clickEnter();
+        scrollByAgain();
+        clickAcceptCookie();
+        bicicleteAdultiOption(6);
+        clickAddToCart();
         checkCart();
-        cleanthecart();
+
+
     }
 
 
